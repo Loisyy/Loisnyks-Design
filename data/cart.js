3e2afa6a2a368261c077741ddfb1cd835e8fe08c
Nkeiru Lois <nkeiruloisy@yahoo.com>
2025-03-30 19:25:43 +0100

    fix price formatting

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];
+export const cart = [{
+    productId:
+    '30d0cb8e-0e98-4a01-9c2c-cc097c878716',
+    quantity: 2,
+}, {
+    productId: '1e4713fd-fcef-400b-8a0d-b81ca3f2dfc5',
+    quantity: 1,
+}];
 
 export function addToCart(productId) {
     let matchingItem; 
