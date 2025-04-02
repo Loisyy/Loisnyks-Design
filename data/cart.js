6fe3b4efd51c839f6d6e91db82235b022d811de7
Nkeiru Lois <nkeiruloisy@yahoo.com>
2025-04-02 05:10:16 +0100

    added delivery

--- data/cart.js
+++ data/cart.js
@@ -6,9 +6,11 @@ if (!cart) {
         productId:
         '30d0cb8e-0e98-4a01-9c2c-cc097c878716',
         quantity: 2,
+        deliveryOptionId: '1'
     }, {
         productId: '1e4713fd-fcef-400b-8a0d-b81ca3f2dfc5',
         quantity: 1,
+        deliveryOptionId: '2'
     }];
 }
 function saveToStorage() {
@@ -29,7 +31,8 @@ export function addToCart(productId) {
     } else {
         cart.push({
             productId: productId,
-            quantity: 1
+            quantity: 1,
+            deliveryOptionId: '1'
         });
     }
 
@@ -40,4 +43,19 @@ export function removeFromCart(productId) {
     const newCart = cart.filter(cartItem => cartItem.productId !== productId); // ✅ More efficient filtering
     cart = newCart;
     saveToStorage(); // ✅ Ensure cart update is saved
+}
+
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId
+
+    saveToStorage();
 }
